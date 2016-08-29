86441231e466f60067e3952af9b97f1c49b19619
rachna-chauhan <rachanachauhan.14@gmail.com>
2016-08-29 17:38:28 +0530

    Add files via upload

---
+++ rachna 1.py
@@ -0,0 +1,11 @@
+x=int(input("enter 1st num:"))
+y=int(input("enter 2nd num:"))
+i=x
+while i<=y:
+    if(i%5==0):
+        print(i,"is multiple of 5")
+    else:
+        print(i,"is not multiple of 5")
+    i=i+1
+print("biee")
+                                         
